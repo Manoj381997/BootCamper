8f8ce65d8f0befe3301175b2eaccf6e25982f2af
Manoj381997 <gmanojk84@gmail.com>
2020-12-19 23:41:21 +0530

    Adds mongodb and mongoose connection along with swagger documentation

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 // const logger = require('./middleware/logger');
-
-// Import Routers
-const bootcampsRouter = require('./routers/bootcamps.router');
+const colors = require('colors');
+const connectDb = require('./config/db');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDb();
+
+// Import Routers
+const bootcampsRouter = require('./routers/bootcamps.router');
 
 const app = express();
 const API = '/api/v1';
@@ -24,11 +31,27 @@ app.get('/', (req, res) => {
   });
 });
 
+// Load Swagger
+app.use(
+  API + '/swagger',
+  swaggerUi.serve,
+  swaggerUi.setup(YAML.load('./swagger.yml'))
+);
+
 // Mount Routers
 app.use(API + '/bootcamps', bootcampsRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  // close server and exit process
+  server.close(() => process.exit(1)); // 1 -> close with failure
+});
